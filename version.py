e97f62f95c915f632b381a4a547fa94988793ce5
Oliver Tiemann <oliver.tiemann@schule.hessen.de>
2025-08-24 11:36:32 +0200

    Bump version to v0.1.9-beta

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 Version management for py-aufsichtsplan
 """
 
-__version__ = "0.1.8-beta"
-__build_date__ = "2025-08-24"
+__version__ = "0.1.9-beta"
+__build_date__ = "2025-01-24"
 
 def get_version_info():
     """Returns version information as dictionary"""
